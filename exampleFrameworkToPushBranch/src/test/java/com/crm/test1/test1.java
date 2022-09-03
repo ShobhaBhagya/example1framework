c50c323a241a12bd9fc6ae0014fa71bee09f3e48
shash <shash@SHASHANK>
2022-09-03 13:14:24 +0530

    I commit example framework

---
+++ exampleFrameworkToPushBranch/src/test/java/com/crm/test1/test1.java
@@ -0,0 +1,27 @@
+package com.crm.test1;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+
+	@Test
+	public void createcontct()
+	{
+		System.out.println("contact created");
+		}
+	
+	@Test
+	public void modifycontct()
+	{
+		System.out.println("modified contact");
+	}
+	
+//	@Test
+//	public void deletecontact()
+//	{
+//		System.out.println("modified contact");
+//	}
+	
+
+}
